c35900253f93e83536096eb4e8b4658529c38172
Lanfor <Lanfor@LanforMenaMontalbn>
2016-05-24 11:35:49 -0600

    Merge origin/master
    Conflicts:
    	Curso.xml
    	Estudiante.xml
    	Matricula.xml
    	Usuario.xml

---
+++ src/controlador/Controlador_FRM_EntradaJuego.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import modelo.Hilo_Espera;
+import vista.FRM_EntradaJuego;
+//import modelo.Hilo_Espera;
+import modelo.MetodosRecord;
+import vista.FRM_Espera;
+import vista.FRM_VentanaJuego;
+//import modelo.Record;
+//import vista.FRM_EntradaJuego;
+//import vista.FRM_Espera;
+//import vista.FRM_VentanaJuego;
+
+public class Controlador_FRM_EntradaJuego implements ActionListener
+{
+    FRM_EntradaJuego fRM_EntradaJuego;
+    public FRM_VentanaJuego fRM_VentanaJuego;
+    public FRM_Espera fRM_Espera;
+    Hilo_Espera hilo_Espera;
+    
+    public MetodosRecord metodosRecord;
+    
+    public Controlador_FRM_EntradaJuego(FRM_EntradaJuego fRM_EntradaJuego)
+    {
+        metodosRecord=new MetodosRecord();
+        this.fRM_EntradaJuego=fRM_EntradaJuego;
+        metodosRecord.ordenar(fRM_EntradaJuego);
+        this.fRM_VentanaJuego=new FRM_VentanaJuego(fRM_EntradaJuego);
+        this.fRM_Espera=new FRM_Espera();
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        switch(e.getActionCommand())
+        {
+            case "Jugar":
+                hilo_Espera=new Hilo_Espera(this);
+                hilo_Espera.start();
+            break;
+            case "Salir":
+                System.exit(0);
+            break;
+            case "Acerca De":
+                System.out.println("Acerca De");
+            break;
+            case "Sonido":
+                fRM_EntradaJuego.cambiarEstadoMusica();
+            break;
+        }
+    }
+    
+}
---
+++ src/modelo/Hilo.java
@@ -0,0 +1,45 @@
+package modelo;
+
+import vista.FRM_VentanaJuego;
+
+/**
+ *
+ * @author Lanfor
+ */
+public class Hilo extends  Thread
+{
+    FRM_VentanaJuego ventana;
+    int tiempo=0, timeMusic=0;
+    public Hilo(FRM_VentanaJuego ventana) 
+    {
+        this.ventana=ventana;
+    }
+    
+    public void run()
+    {
+        while (true) 
+        {
+            try 
+            {
+                this.sleep(100);
+                ventana.moverZombies();
+                aumentarTiempo();
+            } catch (Exception e) 
+            {
+                System.err.println("Hubo en error en el hilo de ejecucion run: "+e.getCause());
+            }
+            
+        }
+    }
+    
+    public void aumentarTiempo()
+    {
+        
+        if(tiempo==20)
+        {
+            ventana.timer();
+            tiempo=0;
+        }
+            tiempo++;
+    }
+}
